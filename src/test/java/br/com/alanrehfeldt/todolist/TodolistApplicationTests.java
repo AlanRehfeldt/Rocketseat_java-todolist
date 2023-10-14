d3ef493f2d824f513821bb48a29dae0ad2fb49b5
Alan Rehfeldt <alanrehfeldt@MacBook-Pro-de-Alan.local>
2023-10-14 19:03:43 -0300

    first commit

---
+++ src/test/java/br/com/alanrehfeldt/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alanrehfeldt.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
